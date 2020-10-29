7b605028495ecea2afb5efa4b1277050d461e58a
Olivier Levitt <ungawa14@gmail.com>
2020-10-29 10:21:11 +0100

    Initial commit

---
+++ src/main/java/io/insee/dev/k8sonboarding/K8sOnboardingApplication.java
@@ -0,0 +1,13 @@
+package io.insee.dev.k8sonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/insee/dev/k8sonboarding/K8sOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.insee.dev.k8sonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
