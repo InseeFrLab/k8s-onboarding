da2ddbba2d82ce244db1935b0296c154b44d57d7
YMLG <yves-marie.le-gueut@insee.fr>
2020-10-30 11:41:50 +0100

    close client

--- src/main/java/io/insee/dev/k8sonboarding/service/OnboardingService.java
+++ src/main/java/io/insee/dev/k8sonboarding/service/OnboardingService.java
@@ -16,28 +16,30 @@ import io.insee.dev.k8sonboarding.model.User;
 public class OnboardingService {
 
     public void onboard(User user) {
-	final KubernetesClient client = new DefaultKubernetesClient();
-	final String userId = user.getId();
-	final String namespaceId = "prefix" + "-" + user.getId();
-	final DoneableNamespace namespaceToCreate = client.namespaces().createNew().withNewMetadata()
-		.withName(namespaceId).addToLabels("created_by", "k8s-onboarding").endMetadata();
-
-	final DoneableRoleBinding bindingToCreate = client.rbac().roleBindings().inNamespace(namespaceId).createNew()
-		.withNewMetadata().withLabels(Map.of("created_by", "k8s-onboarding")).withName("namespace_admin")
-		.withNamespace(namespaceId).endMetadata()
-		.withSubjects(new SubjectBuilder().withKind("User").withName(userId)
-			.withApiGroup("rbac.authorization.k8s.io").withNamespace(namespaceId).build())
-		.withNewRoleRef().withApiGroup("rbac.authorization.k8s.io").withKind("ClusterRole")
-		.withName("cluster-admin").endRoleRef();
-
-	namespaceToCreate.done();
-	bindingToCreate.done();
+      try(final KubernetesClient client = new DefaultKubernetesClient()){
+    	final String userId = user.getId();
+    	final String namespaceId = "prefix" + "-" + user.getId();
+    	final DoneableNamespace namespaceToCreate = client.namespaces().createNew().withNewMetadata()
+    		.withName(namespaceId).addToLabels("created_by", "k8s-onboarding").endMetadata();
+    
+    	final DoneableRoleBinding bindingToCreate = client.rbac().roleBindings().inNamespace(namespaceId).createNew()
+    		.withNewMetadata().withLabels(Map.of("created_by", "k8s-onboarding")).withName("namespace_admin")
+    		.withNamespace(namespaceId).endMetadata()
+    		.withSubjects(new SubjectBuilder().withKind("User").withName(userId)
+    			.withApiGroup("rbac.authorization.k8s.io").withNamespace(namespaceId).build())
+    		.withNewRoleRef().withApiGroup("rbac.authorization.k8s.io").withKind("ClusterRole")
+    		.withName("cluster-admin").endRoleRef();
+    
+    	namespaceToCreate.done();
+    	bindingToCreate.done();
+      }
     }
 
     public Boolean checkNamespaceExists(User user) {
-	final KubernetesClient client = new DefaultKubernetesClient();
-	final String namespaceId = "prefix" + "-" + user.getId();
-	final Namespace namespace = client.namespaces().withName(namespaceId).get();
-	return namespace == null ? Boolean.FALSE : Boolean.TRUE;
+      try(final KubernetesClient client = new DefaultKubernetesClient()){
+        final String namespaceId = "prefix" + "-" + user.getId();
+        final Namespace namespace = client.namespaces().withName(namespaceId).get();
+        return namespace == null ? Boolean.FALSE : Boolean.TRUE;
+      }
     }
 }
