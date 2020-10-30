3ef9056d5e1d800c931614f0f89ac7297dc8f4a1
Donatien Eneman <donatien.eneman@insee.fr>
2020-10-30 10:04:24 +0100

    fix

--- ui/src/components/commons/header.tsx
+++ ui/src/components/commons/header.tsx
@@ -2,11 +2,11 @@ import { useKeycloak } from '@react-keycloak/web';
 import D from 'i18n';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
+import {AppBar,Button} from '@material-ui/core/';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import PersonOutlineSharpIcon from '@material-ui/icons/PersonOutlineSharp';
 
 const Header = () => {
 	const {
@@ -29,12 +29,14 @@ const Header = () => {
 		<>
 			<AppBar position="static">
 				<Toolbar variant="dense">
-					<IconButton edge="start" color="inherit" aria-label="menu">
-						<MenuIcon />
-					</IconButton>
 					<Typography variant="h6" color="inherit">
 						{D.appTitle}
 					</Typography>
+					<Button
+        variant="contained"
+        color="default"
+        startIcon={<PersonOutlineSharpIcon />}
+      >
 				</Toolbar>
 			</AppBar>
 			<button type="button" onClick={action}>
