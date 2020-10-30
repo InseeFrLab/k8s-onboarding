aa4316651811c3abf7823a97f49ae6a2b52cc696
Donatien Eneman <45652587+Donatien26@users.noreply.github.com>
2020-10-30 10:07:39 +0100

    Package json forgot (#25)
    * setup material ui
    
    * add package.json
    
    * fix
    
    * final doneé
    
    Co-authored-by: Donatien Eneman <donatien.eneman@insee.fr>

--- ui/src/components/commons/header.tsx
+++ ui/src/components/commons/header.tsx
@@ -2,11 +2,9 @@ import { useKeycloak } from '@react-keycloak/web';
 import D from 'i18n';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
+import { AppBar } from '@material-ui/core/';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
 	const {
@@ -29,9 +27,6 @@ const Header = () => {
 		<>
 			<AppBar position="static">
 				<Toolbar variant="dense">
-					<IconButton edge="start" color="inherit" aria-label="menu">
-						<MenuIcon />
-					</IconButton>
 					<Typography variant="h6" color="inherit">
 						{D.appTitle}
 					</Typography>
