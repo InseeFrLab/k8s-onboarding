1dde4ee5340bee763468c883b0094761d6660fee
NicoLaval <nicolas.laval@insee.fr>
2020-10-30 09:29:09 +0100

    Improve home component

--- ui/src/i18n/dictionary.ts
+++ ui/src/i18n/dictionary.ts
@@ -5,6 +5,10 @@ export const dictionary = {
 			"Déployer n'importe quelle application conteneurisée en décrivant complètement sa configuration, les différents modules applicatifs et leurs interactions",
 		en: 'Deploy ...',
 	},
+	homeBody: {
+		fr: 'Connectez-vous pour accéder à vos information',
+		en: 'Login to access to your informations',
+	},
 	loading: { fr: 'Chargement ...', en: 'Loading...' },
 	login: { fr: 'Se connecter', en: 'Login' },
 	logout: { fr: 'Se déconnecter', en: 'Logout' },
