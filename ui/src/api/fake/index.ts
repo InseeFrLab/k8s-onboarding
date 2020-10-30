7ca2d0fb17a8637f060577d56814cce3f1f87c80
NicoLaval <nicolas.laval@insee.fr>
2020-10-30 07:56:12 +0100

    Improve ui API calls

--- ui/src/api/fake/index.ts
+++ ui/src/api/fake/index.ts
@@ -1,9 +1,15 @@
-interface API {
-	conf: Promise<any>;
-}
+import { API } from '../api';
+
+const get = (data: any) => Promise.resolve(data);
 
 const api: API = {
-	conf: Promise.resolve({ test: 'test' }),
+	conf: get({ test: 'test' }),
+	cluster: get({
+		name: 'google-cloud-demo',
+		'api-server': 'https://oidc.dev.insee.io',
+		token: 'my-token',
+		script: 'script content',
+	}),
 };
 
 export default api;
