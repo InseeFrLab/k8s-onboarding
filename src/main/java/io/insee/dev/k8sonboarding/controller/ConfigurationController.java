09f78fc44c04c6c2a29ce71fdee89877abfca393
alexisdondon <alexis.dondon@insee.fr>
2020-10-30 15:47:17 +0100

    un s qui traine

--- src/main/java/io/insee/dev/k8sonboarding/controller/ConfigurationController.java
+++ src/main/java/io/insee/dev/k8sonboarding/controller/ConfigurationController.java
@@ -20,7 +20,7 @@ public class ConfigurationController {
     @Autowired
     ClusterProperties clusterProperties;
 
-    @Autowireds
+    @Autowired
     UiProperties uiProperties;
 
     @GetMapping
