52e77d4cb00a60f7cdf43cc9683c27b5b830ab9c
Julien Lhotelier <smith.julien@gmail.com>
2022-10-17 11:17:17 +0200

    Remove use of deprecated interface WebSecurityConfigurerAdapter

--- src/main/java/io/insee/dev/k8sonboarding/configuration/security/SecurityConfiguration.java
+++ src/main/java/io/insee/dev/k8sonboarding/configuration/security/SecurityConfiguration.java
@@ -1,26 +1,26 @@
 package io.insee.dev.k8sonboarding.configuration.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-	http.httpBasic().disable().csrf().disable()
-	    .authorizeRequests()
-			.antMatchers("/api/public/**").permitAll()
-			.antMatchers("/api","/api/").permitAll() // For swagger-ui redirection
-		    .antMatchers("/api/**").authenticated()
-		    .anyRequest().permitAll()
-		.and()
-			.oauth2ResourceServer()
-			.jwt();
-	}
-	
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic().disable().csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/api/public/**").permitAll()
+            .antMatchers("/api","/api/").permitAll() // For swagger-ui redirection
+            .antMatchers("/api/**").authenticated()
+            .anyRequest().permitAll()
+        .and()
+            .oauth2ResourceServer()
+            .jwt();
+        return http.build();
+    }
 
-	
+}    
   
-}
