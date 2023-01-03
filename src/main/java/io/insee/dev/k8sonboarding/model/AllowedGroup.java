0766019b32f33e4d18f3e512359ef5c965636363
Antoine Brunetti <antoine.brunetti@insee.fr>
2023-01-03 14:11:27 +0100

    forgot getter & setters

--- src/main/java/io/insee/dev/k8sonboarding/model/AllowedGroup.java
+++ src/main/java/io/insee/dev/k8sonboarding/model/AllowedGroup.java
@@ -1,8 +1,24 @@
 package io.insee.dev.k8sonboarding.model;
 
 public class AllowedGroup {
-    String namespace;
-    String group;
+    private String namespace;
+    private String group;
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
 
     public AllowedGroup(String namespace, String group) {
         this.namespace = namespace;
